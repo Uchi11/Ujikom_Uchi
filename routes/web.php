b4d094cb3ff8eccd336d2d30bd0fd13e0b8bd907
Uchi11 <uchimuhammadr@gmail.com>
2025-02-12 10:34:58 +0700

    Menyusun Routing & Membuat folder dan file di views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Membuat route untuk home
+Route::get('/', [TaskController::class, 'index'])->name('home');
+
+// Membuat route resource untuk Task List
+Route::resource('lists', TaskListController::class);
+
+// Membuat route resource untuk Task
+Route::resource('tasks', TaskController::class);
+
+// Membuat route untuk menyelesaikan task
+Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
